47af98c89f3d6266f5492c70edd5384e892bcc1b
0xhank <zeroxhank@gmail.com>
2024-03-05 16:54:12 -0500

    feat: style improvements

--- src/CheatcodesList.tsx
+++ src/CheatcodesList.tsx
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import { useState } from "react";
 import { Cheatcodes } from "./types";
 
@@ -52,58 +53,62 @@ export const CheatcodesList = ({ cheatcodes }: { cheatcodes: Cheatcodes }) => {
 
   return (
     <div
-      className="overflow-auto px-4 py-2 bg-black-800/50 text-gray-400 h-full pointer-events-auto"
+      className="overflow-auto flex flex-col gap-1 p-2 bg-black-800/50 text-gray-400 h-full pointer-events-auto"
       style={{ background: "rgba(0,0,0,0.2)" }}
     >
-      {Object.entries(cheatcodes).map(([funcName], i) => {
-        return (
-          <div
-            key={`function-${funcName}-${i}`}
-            className="py-2 border-b border-gray-400 flex flex-col gap-2"
-          >
-            <h1 className="text-white opacity-90 font-bold text-sm">
-              {camelCaseToCapitalized(funcName)}
-            </h1>{" "}
-            {cheatcodes[funcName].params.length > 0 && (
-              <div className="grid grid-cols-[max-content,1fr] gap-2">
-                {(cheatcodes[funcName].params || []).map((param, index) => (
-                  <div
-                    key={`param-input-${param.name}-${index}`}
-                    className="flex gap-3 items-center"
-                  >
-                    <div className="text-white opacity-80">
-                      {camelCaseToCapitalized(param.name)}
-                    </div>
-                    <input
-                      type={getTypeInput(param.type)}
-                      placeholder={param.name}
-                      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                      // @ts-ignore
-                      value={params[funcName]?.[param.name] ?? ""}
-                      onChange={(e) =>
-                        handleParamChange(
-                          funcName,
-                          param.name,
-                          e.target.type === "checkbox"
-                            ? e.target.checked
-                            : e.target.value
-                        )
-                      }
-                      className="border rounded-sm p-1 w-full text-xs text-black bg-slate-200"
-                    />
+      {_.chunk(Object.entries(cheatcodes), 2).map((chunk, i) => (
+        <div className="grid grid-cols-2 gap-1">
+          {chunk.map(([funcName], i) => {
+            return (
+              <div
+                key={`function-${funcName}-${i}`}
+                className="bg-slate-700 rounded-md flex flex-col p-2 justify-between gap-1"
+              >
+                <h1 className="text-white opacity-90 font-bold text-sm">
+                  {camelCaseToCapitalized(funcName)}
+                </h1>{" "}
+                {cheatcodes[funcName].params.length > 0 && (
+                  <div className="grid grid-cols-[max-content,1fr] gap-2">
+                    {(cheatcodes[funcName].params || []).map((param, index) => (
+                      <div
+                        key={`param-input-${param.name}-${index}`}
+                        className="flex gap-3 items-center"
+                      >
+                        <div className="text-xs text-white opacity-80">
+                          {camelCaseToCapitalized(param.name)}
+                        </div>
+                        <input
+                          type={getTypeInput(param.type)}
+                          placeholder={param.name}
+                          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                          // @ts-ignore
+                          value={params[funcName]?.[param.name] ?? ""}
+                          onChange={(e) =>
+                            handleParamChange(
+                              funcName,
+                              param.name,
+                              e.target.type === "checkbox"
+                                ? e.target.checked
+                                : e.target.value
+                            )
+                          }
+                          className="border rounded-sm p-1 w-full text-xs text-black bg-slate-200"
+                        />
+                      </div>
+                    ))}
                   </div>
-                ))}
+                )}
+                <button
+                  onClick={() => executeFunction(funcName)}
+                  className="bg-slate-600 text-white py-1 rounded-sm focus:outline-none focus:ring focus:border-blue-100"
+                >
+                  Run
+                </button>
               </div>
-            )}
-            <button
-              onClick={() => executeFunction(funcName)}
-              className="bg-slate-600 text-white py-1 rounded-sm focus:outline-none focus:ring focus:border-blue-100"
-            >
-              Run
-            </button>
-          </div>
-        );
-      })}
+            );
+          })}
+        </div>
+      ))}
     </div>
   );
 };
